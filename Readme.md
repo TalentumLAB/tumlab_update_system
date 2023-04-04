864e2b65ecd389dea0e1c27792313b8efc4db393
Jose Ramiro Ramirez Vargas <especialista.servidores@talentum.edu.co>
2023-04-04 14:19:30 -0500

    Add functions to installing multiple patchs

--- Readme.md
+++ Readme.md
@@ -10,7 +10,7 @@
 -   initializer file in the patch archive
 
 ## Description
-This bash file installs various patches left in a TUMLAB zipped folder using an initializer file named init.sh
+This bash file installs several patches that remain in a zipped TUMLAB folder using an initializer file named init.sh, which should be zipped in a folder named "p+patch number" example: "p1"
 
 ## How to use
 
--- updater.sh
+++ updater.sh
@@ -27,53 +27,66 @@ count_patch=$(ls "$path_find_patch" | wc -l)
 logger -p local0.debug -it tumlab_update_system "Validating new patches"
 
 echo "$count_patch"
-
+  
 if [[ "$count_patch" = 0 ]]; then
 
     echo "No patches available"
     logger -p local0.debug -it tumlab_update_system "No patches available"
 else
-    echo "Installing patch"
-    logger -p local0.debug -it tumlab_update_system "New patches available"
-    logger -p local0.debug -it tumlab_update_system "Installing patch"
 
-    patch_file=$(find "$path_find_patch" -type f -iname "*.zip")
+    find $path_find_patch -type f -iname "*.zip" > temp.txt
 
-    unzip "$patch_file" -d "$path_find_patch"
-    logger -p local0.debug -it tumlab_update_system "Unzipping patch folder"
+    while IFS= read -r line
+    do
+        echo "Installing patch"
+        logger -p local0.debug -it tumlab_update_system "New patches available"
+        logger -p local0.debug -it tumlab_update_system "Installing patch"
 
-    patch_folder=$(find "$path_find_patch"* -type d)
-    echo "$patch_folder"
+        patch_file=$line
 
-    patch_exec_file="$patch_folder/init.sh"
-    exist_init=$(checkExitsFile "$patch_exec_file")
-    if [[ $exist_init == 'false' ]]; then
-        logger -p local0.debug -it tumlab_update_system "No init file"
-        echo "No init file"
-    else
-        echo "Execute init file"
-        logger -p local0.debug -it tumlab_update_system "Execute init file $patch_exec_file"
-        chmod +x "$patch_exec_file"
-        echo "$patch_exec_file"
-        $patch_exec_file >> /tumlab/syncthing/log_execution_updater.log
-        check_error="$?"
-        logger -p local0.debug -it tumlab_update_system "Checking error to execute init file. Exit code:$check_error"
-        echo "$check_error"
-        if [[ check_error -eq 0 ]]; then
+        unzip "$patch_file" -d "$path_find_patch"
+        logger -p local0.debug -it tumlab_update_system "Unzipping patch folder"
 
-            logger -p local0.debug -it tumlab_update_system "Successful patch installation"
-            cp "$patch_file" "$history_patches"
-            logger -p local0.debug -it tumlab_update_system "Copying patch files in the history folder patches"
-            rm "$patch_file"
+        # patch_folder=$(find "$path_find_patch"* -type d)
+        file_name=$(echo "$line" | awk -F '/' '{print $5}'| awk -F '.' '{print $1}')
+        patch_folder="$path_find_patch/$file_name"
+        echo "$patch_folder"
+
+        patch_exec_file="$patch_folder/init.sh"
+        exist_init=$(checkExitsFile "$patch_exec_file")
+        if [[ $exist_init == 'false' ]]; then
+            logger -p local0.debug -it tumlab_update_system "No init file"
+            echo "No init file"
             rm -r "$patch_folder"
-            logger -p local0.debug -it tumlab_update_system "Deleting patches file to syncthing folder"
         else
-            echo "Error installing patch"
-            logger -p local0.debug -it tumlab_update_system "Error installing patch. Exit code=$check_error"
-            rm -r "$patch_folder"
+            echo "Execute init file"
+            logger -p local0.debug -it tumlab_update_system "Execute init file $patch_exec_file"
+            chmod +x "$patch_exec_file"
+            echo "$patch_exec_file"
+            $patch_exec_file >> /tumlab/syncthing/log_execution_updater.log
+            check_error="$?"
+            logger -p local0.debug -it tumlab_update_system "Checking error to execute init file. Exit code:$check_error"
+            echo "$check_error"
+            if [[ check_error -eq 0 ]]; then
+
+                logger -p local0.debug -it tumlab_update_system "Successful patch installation"
+                cp "$patch_file" "$history_patches"
+                logger -p local0.debug -it tumlab_update_system "Copying patch files in the history folder patches"
+                rm "$patch_file"
+                rm -r "$patch_folder"
+                logger -p local0.debug -it tumlab_update_system "Deleting patches file to syncthing folder"
+            else
+                echo "Error installing patch"
+                logger -p local0.debug -it tumlab_update_system "Error installing patch. Exit code=$check_error"
+                rm -r "$patch_folder"
+                break
+
+            fi
 
         fi
+        
+    done < temp.txt
+    rm temp.txt
 
-    fi
 fi
 logger -p local0.debug -it tumlab_update_system "End tumlab update process"
